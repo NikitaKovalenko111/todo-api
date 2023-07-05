d55a29a7a00efa752554dd754c0a11942d26e533
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2023-07-06 02:02:06 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,16 @@ import express, { Request, Response, Express } from "express"
 import bodyParser from "body-parser"
 import { todoRouter } from "./routes/todo-router"
 import { Statuses } from "./types"
+import cors from 'cors'
 
 const server: Express = express()
 const port: string | number = process.env.PORT || 3000
 
 const parseMiddleware = bodyParser({})
+server.use(cors())
 server.use(parseMiddleware)
 server.use('/todos', todoRouter)
 
 server.get('/', (req: Request, res: Response) => {
   res.send(Statuses.NotFound)
-})
-
-server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
