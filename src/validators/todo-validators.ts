8db73e280091f58e9c03086cbd9f11df167bfd10
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2023-07-05 22:58:54 +0500

    dist

--- src/validators/todo-validators.ts
+++
@@ -1,7 +0,0 @@
-export const targetValidator = (target: string): string => {
-    if (!target.trim()) 
-        return "value is required!"
-    else if (target.length > 20) 
-        return "value is more than 20 symbols!"
-    return target
-}
